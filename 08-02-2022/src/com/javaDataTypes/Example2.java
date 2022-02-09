fe2095aff0dff5a3bedd245c8934361ec3519cc4
nikitha <nikitha@LAPTOP-4GAKUODQ>
2022-02-09 16:40:35 +0530

    java commit

---
+++ 08-02-2022/src/com/javaDataTypes/Example2.java
@@ -0,0 +1,20 @@
+package com.javaDataTypes;
+
+public class Example2 {
+
+	public static void main(String[] args) 
+	{
+		//int studentages = 21,23,25,26
+		//System.out.println(studentages);
+		
+		int studentages[] = {21,23,25,26};
+		//System.out.println(studentages[]);
+		
+		for(int studentage: studentages)
+		{
+			System.out.println(studentage);
+		}
+
+	}
+
+}
